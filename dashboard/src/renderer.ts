e87983087c7213a6425406fad14511ff371e2151
gbdelgado <gdelgado1@email.arizona.edu>
2022-02-16 16:07:00 -0700

    removing old dashes

--- dashboard/src/renderer.ts
+++ dashboard/src/renderer.ts
@@ -1,75 +1,67 @@
 import "./index.css";
 import $ from "jquery";
 import {
-	CarData,
-	CarError,
-	ErrorEmitter,
-	Slot,
-	DashColors,
+    CarData,
+    CarError,
+    DashColors,
     IPCEvents,
+    MAX_RPM,
 } from "./utils/dash-types";
 import { ipcRenderer, IpcRendererEvent } from "electron";
+import { linearScale } from "./utils/dash-utils";
 
-// SLOTS
-const tlSlot: Slot = {
-	text: $("#slot-tl-text"),
-	label: $("#slot-tl-label"),
-};
-const trSlot: Slot = {
-	text: $("#slot-tr-text"),
-	label: $("#slot-tr-label"),
-};
-const blSlot: Slot = {
-	text: $("#slot-bl-text"),
-	label: $("#slot-br-label"),
-};
-const brSlot: Slot = {
-	text: $("#slot-br-text"),
-	label: $("#slot-br-label"),
-};
-const centerSlot: Slot = {
-	text: $("#slot-center-text"),
-};
+const gearContainer = $("#slot-center");
+const rpmContainer = $("#slot-center-bottom");
+const throttleText = $("#slot-value-left-1");
+const coolantText = $("#slot-value-left-2");
+const batteryText = $("#slot-value-right-1");
+const lapText = $("#slot-value-right-2");
+const errorContainer = $("#slot-error");
+const errorText = $(".error-text");
 
-// Error Emitter
-const errorEmitter: ErrorEmitter = {
-	container: $("#error-emitter-container"),
-	message: $("#error-emitter-message"),
-};
+//@ts-expect-error
+const canvas: HTMLCanvasElement = $("#canvas").get(0);
+const ctx = canvas.getContext("2d");
 
 //////////// EVENTS ////////////
 
-ipcRenderer.on("car-data", (e: IpcRendererEvent, data: CarData) => {
-	if (errorEmitter.container.hasClass("visible")) {
-		errorEmitter.container.removeClass("visible");
-	}
-
-    //color check
-    if (parseInt(data.engineData.rpm) > 10500) {
-        trSlot.text.addClass('red');
-        tlSlot.text.addClass('red');
-        centerSlot.text.addClass('red');
-    } else {
-        trSlot.text.removeClass('red');
-        tlSlot.text.removeClass('red');
-        centerSlot.text.removeClass('red');
+ipcRenderer.on(IPCEvents.CAR_DATA, (e: IpcRendererEvent, data: CarData) => {
+    // remove any disconnection warnings
+    if (errorContainer.hasClass("visible")) {
+        errorContainer.removeClass("visible");
     }
-	// gear
-	centerSlot.text.html(data.engineData.gear);
 
-	// rpm
-	tlSlot.text.html(data.engineData.rpm);
-	tlSlot.label.html("RPM");
+    // fill text slots
+    gearContainer.html(data.engineData.gear);
+    rpmContainer.html(data.engineData.rpm);
+    throttleText.html(data.engineData.throttlePosition);
+    batteryText.html(data.engineData.batteryVoltage);
+    coolantText.html(data.engineData.coolantTemp);
+    lapText.html(data.lapData.currentLap);
 
-	//speed
-	trSlot.text.html(data.engineData.speed);
-	trSlot.label.html("KPH");
+    // rev bar
+    ctx.clearRect(0, 0, 760, 50);
+    ctx.fillStyle = DashColors.GREY;
+    ctx.fillRect(0, 0, 760, 51);
+
+    // map 
+    const fillWidth = linearScale(parseInt(data.engineData.rpm), 0, MAX_RPM, 0, 760);
+    ctx.fillStyle = DashColors.ORANGE;
+    ctx.fillRect(0, 0, fillWidth, 50);
+
+    for (let x = 76; x < 760; x += (760 / 10)) {
+        ctx.beginPath();
+        ctx.moveTo(x, 15);
+        ctx.lineTo(x, 30);
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+    }
+
 });
 
-ipcRenderer.on("car-error", (e: IpcRendererEvent, error: CarError) => {
-    if (!errorEmitter.container.hasClass("visible")) {
-		errorEmitter.container.addClass("visible");
-	}
-	errorEmitter.message.html(error.msg);
+ipcRenderer.on(IPCEvents.CAR_ERROR, (e: IpcRendererEvent, error: CarError) => {
+    errorContainer.addClass("visible");
+    errorText.html(error.msg);
 });
 
